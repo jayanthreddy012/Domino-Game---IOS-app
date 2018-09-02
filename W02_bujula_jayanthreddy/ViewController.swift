d6e7aa6f79e62fc36b48c4c478a52dc81f3c7449
Jayanth Reddy Bujula <jbujula@mac19-cs.ms.okstate.edu>
2018-09-02 15:34:22 -0500

    Initial Commit

---
+++ W02_bujula_jayanthreddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W02_bujula_jayanthreddy
+//
+//  Created by Jayanth Reddy Bujula on 9/2/18.
+//  Copyright © 2018 com.jayanth.bujula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
